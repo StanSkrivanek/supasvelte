47a2eed99e365ae6e7420ab9c10a5ebb08c73800
Stan Skrivanek <s.skrivanek@icloud.com>
2022-10-01 00:06:07 +0100

    ADD: Base64 preview Avatar image and delete functionality

---
+++ src/lib/components/icons/AvatarIcn.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let width = 100,
+		height = 100;
+</script>
+
+<svg {width} {height} fill="none" viewBox="0 0 256 256"
+	><circle
+		cx="128"
+		cy="96"
+		r="64"
+		fill="none"
+		stroke="#000000"
+		stroke-miterlimit="10"
+		stroke-width="16"
+	/><path
+		d="M31,216a112,112,0,0,1,194,0"
+		fill="none"
+		stroke="#000000"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="16"
+	/></svg
+>
--- src/routes/dashboard/instructors/create/+page.svelte
+++ src/routes/dashboard/instructors/create/+page.svelte
@@ -1,50 +1,117 @@
 <script>
+	import AvatarIcn from '$lib/components/icons/AvatarIcn.svelte';
 	import { goto } from '$app/navigation';
 	import { supabase } from '$lib/supabase/supabaseClient';
+	import { onMount } from 'svelte';
 
-	let values = {
-		name: '',
-		phone: '',
-		email: '',
-		bio: '',
-		avatar_url: ''
+		let deleteBtn = undefined;
+		
+		let values = {
+			name: '',
+			phone: '',
+			email: '',
+			bio: '',
+			avatar_url: ''
+		};
+		
+		// ---------------------------------------------------------------------------------
+		
+		const handleFilesUpload = async (e) => {
+		deleteBtn = document.querySelector('#delete-img');
+		const avatarFile = e.target.files[0];
+		createTempImgBase64(avatarFile).then((url) => {
+			values.avatar_url = url;
+		});
+		e.target.value = '';
+		deleteBtn.disabled = false;
 	};
+	// create Base64 image from file upload to display as a image preview
+	function createTempImgBase64(file) {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.onload = (event) => {
+				resolve(event.target.result);
+			};
+			reader.onerror = (error) => {
+				reject(error);
+			};
+			reader.readAsDataURL(file);
+		});
+	}
+	// delete image preview
+	function deleteAvatar() {
+		values.avatar_url = '';
+		deleteBtn.disabled = true;
+	}
+	// delete current image from database
+	// const deleteAvatar = async () => {
+	// 	const { error } = await supabase.storage
+	// 		.from('avatars')
+	// 		.remove([`public/${values.avatar_url}`]);
+
+	// 	if (error) {
+	// 		console.log('Error deleting file: ', error.message);
+	// 	} else {
+	// 		console.log('File deleted successfully!');
+	// 	}
+	// };
+
+
+	// APPEND IMAGE TO DOM
+	// function appendImage(url) {
+	// console.log('🚀 ~ file: +page.svelte ~ line 68 ~ appendImage ~ url', url);
+	// values.avatar_url = url;
+	// const galery = document.querySelector('.galery');
+	// const publicURL = supabase.storage.from('avatars').getPublicUrl(`public/${avatarFile.name}`)
+	// 	.data.publicUrl;
+
+	// values.avatar_url = publicURL;
+	// 	const avatarImg = `
+	// 		<div class="avatar__w">
+	// 			<img class="avatar__img" src="${url}" alt="${values.name}" />
+	// 			<button class="danger" id="delete-img"on:click={deleteAvatar}>Delete</button>
+	// 		</div>
+	// 	`;
+	// galery.innerHTML = avatarImg;
+	// }
+
+	// ---------------------------------------------------------------------------------
 
 	//TODO: SIMPLIFY LOGIC TO IMAGE UPLOAD and PREVIEW
 
-	const handleFilesUpload = async (e) => {
-		const avatarFile = e.target.files[0];
+	// const handleFilesUpload = async (e) => {
+	// 	const avatarFile = e.target.files[0];
 
-		// upload FILE to DB
-		const { error } = await supabase.storage
-			.from('avatars')
-			.upload(`public/${avatarFile.name}`, avatarFile, {
-				cacheControl: '3600',
-				upsert: false
-			});
+	// 	// upload FILE to DB
+	// 	const { error } = await supabase.storage
+	// 		.from('avatars')
+	// 		.upload(`public/${avatarFile.name}`, avatarFile, {
+	// 			cacheControl: '3600',
+	// 			upsert: false
+	// 		});
 
-		if (error) {
-			console.log('Error uploading file: ', error.message);
-		} else {
-			appendImage(avatarFile);
-			console.log('File uploaded successfully!');
-		}
-	};
-	// APPEND IMAGE TO DOM
-	async function appendImage(avatarFile) {
-		const galery = document.querySelector('.galery');
-		const publicURL = supabase.storage.from('avatars').getPublicUrl(`public/${avatarFile.name}`)
-			.data.publicUrl;
+	// 	if (error) {
+	// 		console.log('Error uploading file: ', error.message);
+	// 	} else {
+	// 		appendImage(avatarFile);
+	// 		console.log('File uploaded successfully!');
+	// 	}
+	// };
+	// // APPEND IMAGE TO DOM
+	// async function appendImage(avatarFile) {
+	// 	const galery = document.querySelector('.galery');
+	// 	const publicURL = supabase.storage.from('avatars').getPublicUrl(`public/${avatarFile.name}`)
+	// 		.data.publicUrl;
 
-		values.avatar_url = publicURL;
-		const avatarImg = `
-			<div class="avatar__w">
-				<img class="avatar__img" src="${publicURL}" alt="${avatarFile.name}" />
-				<button class="danger" id="delete-img"on:click={deleteAvatar}>Delete</button>
-			</div>
-		`;
-		galery.innerHTML = avatarImg;
-	}
+	// 	values.avatar_url = publicURL;
+	// 	const avatarImg = `
+	// 		<div class="avatar__w">
+	// 			<img class="avatar__img" src="${publicURL}" alt="${avatarFile.name}" />
+	// 			<button class="danger" id="delete-img"on:click={deleteAvatar}>Delete</button>
+	// 		</div>
+	// 	`;
+	// 	galery.innerHTML = avatarImg;
+	// }
 
 	async function handleSubmit() {
 		await supabase.from('instructors').insert({
@@ -96,15 +163,37 @@
 				bind:value={values.excerpt}
 				placeholder="type your content here"
 			/>
-			<div class="galery" />
+			<div class="galery">
+				<div class="avatar__w">
+					{#if values.avatar_url === ''}
+						<AvatarIcn />
+					{/if}
+					{#if values.avatar_url !== ''}
+						<img class="avatar__img" src={values.avatar_url} alt={values.name} />
+					{/if}
+					<button
+						disabled="true"
+						class="danger"
+						type="button"
+						id="delete-img"
+						on:click={deleteAvatar}>Delete</button
+					>
+				</div>
+
+				<!-- <img
+						class="avatar__img"
+						src={values.avatar_url}
+						alt={values.name}
+					/> -->
+			</div>
 			<label for="avatar"
 				>Profile Image <span>max dimension: 256px x 256px<i>(jpg, webp, avif)</i></span></label
 			>
 			<input
 				type="file"
 				name="avatar"
-				id="avatar_url"
-				accept="image/*,video/*,audio/*,.pdf,.svg,.doc,.docx,.txt"
+				id="avatarInput"
+				accept=".jpg, .jpeg, .png, .webp, .avif, .svg"
 				on:change={(e) => {
 					handleFilesUpload(e);
 				}}
@@ -165,8 +254,18 @@
 		background-color: #1b0e30;
 		color: #fff;
 	}
+	button:disabled {
+		background-color: #ccc;
+		color: #fff;
+		pointer-events: none;
+	}
+	.danger {
+		background-color: var(--col-danger);
+	}
+	/* .info {
+		background-color: var(--col-active);
+	} */
 	.btn-form-xxl h2 {
 		margin-bottom: 0.25rem;
 	}
-
 </style>
