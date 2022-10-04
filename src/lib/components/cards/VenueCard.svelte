516709eb7a533a96b7af1a8322d6e139bc578088
Stan Skrivanek <s.skrivanek@icloud.com>
2022-10-05 00:20:30 +0100

    REF: Create component VenueCard and refactor Venues page

---
+++ src/lib/components/cards/VenueCard.svelte
@@ -0,0 +1,75 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+  
+	export let id;
+	export let name;
+  export let adr_1;
+  export let adr_2;
+  export let city;
+  export let eircode;
+  export let contact;
+  export let phone;
+	export let email;
+	export let website;
+  export let info;
+
+
+</script>
+<div class="db-item" id={id}>
+	<p>{id}</p>
+	<h2>{name}</h2>
+	<div class="col dsb-venue--header">
+		<div class="col">
+			<p>{adr_1}</p>
+			<p>{adr_2}</p>
+			<p>{city}</p>
+			<p>{eircode}</p>
+		</div>
+		<div class="col">
+			<p>{contact}</p>
+			<p>{phone}</p>
+			<p>{email}</p>
+			<p>{website}</p>
+		</div>
+	</div>
+	<!-- <div class="dsb-venue--body">
+						<p>{item.info}</p>
+					</div> -->
+	<div class="col dsb-venue--footer">
+		<!-- <button class="info" on:click={findItemById}>Edit</button> -->
+    	<button class="info" on:click={() => dispatch('edit')}>Edit</button>
+		<!-- <button class="danger" on:click={openDeleteConfirmModal}>Delete</button> -->
+    <button class="danger" on:click>Delete</button>
+	</div>
+</div>
+
+<style>
+
+	.db-item {
+		display: flex;
+		flex-grow: 1;
+		flex-basis: 400px;
+		flex-direction: column;
+		justify-content: space-between;
+		border: #ccc 1px solid;
+		padding: 1rem;
+		border-radius: 0.25rem;
+	}
+	.db-item h2 {
+		margin-bottom: 1rem;
+	}
+	.dsb-venue--header {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 1rem;
+		line-height: 1.2;
+	}
+	/* .dsb-venue--body {
+		margin-bottom: 1rem;
+		line-height: 1.4;
+	} */
+	.dsb-venue--footer {
+		text-align: right;
+	}
+</style>
--- src/routes/dashboard/venues/+page.svelte
+++ src/routes/dashboard/venues/+page.svelte
@@ -7,6 +7,7 @@
 	import Modal from '$lib/components/shared/modals/Modal.svelte';
 	import Plus from '$lib/components/icons/Plus.svelte';
 	import Search from '$lib/components/shared/formfields/Search.svelte';
+	import VenueCard from '$lib/components/cards/VenueCard.svelte';
 	import DeleteConfirm from '$lib/components/shared/modals/DeleteConfirm.svelte';
 
 	export let data;
@@ -32,15 +33,9 @@
 		itemTarget.remove();
 	}
 
-	async function findItemById(e) {
-		const elm = e.target.closest('.db-item');
-		const elmId = elm.id;
-		console.log("🚀 ~ file: +page.svelte ~ line 38 ~ findItemById ~ elmId", elmId)
-		// get data from db table `venues` where id = elmId
-		$itemData = await supabase.from('venues').select('*').match({ id: elmId });
-		// store venue RTE data as string in localStorage
+	async function findItemById(id) {
+		$itemData = await supabase.from('venues').select('*').match({ id: id });
 		localStorage.setItem('itemData', JSON.stringify($itemData));
-		// redirect to update page
 		goto('/dashboard/venues/edit');
 	}
 
@@ -86,32 +81,22 @@
 	</section>
 	<section>
 		<div class="grid_test">
-			{#each sorted as item (item.id)}
-				<div class="db-item" id={item.id}>
-					<p>{item.id}</p>
-					<h2>{item.name}</h2>
-					<div class="col dsb-venue--header">
-						<div class="col">
-							<p>{item.adr_1}</p>
-							<p>{item.adr_2}</p>
-							<p>{item.city}</p>
-							<p>{item.eircode}</p>
-						</div>
-						<div class="col">
-							<p>{item.contact}</p>
-							<p>{item.phone}</p>
-							<p>{item.email}</p>
-							<p>{item.website}</p>
-						</div>
-					</div>
-					<!-- <div class="dsb-venue--body">
-						<p>{item.info}</p>
-					</div> -->
-					<div class="col dsb-venue--footer">
-						<button class="info" on:click={findItemById}>Edit</button>
-						<button class="danger" on:click={openDeleteConfirmModal}>Delete</button>
-					</div>
-				</div>
+			{#each sorted as { id, name, adr_1, adr_2, city, eircode, contact, phone, email, website, info }}
+				<VenueCard
+					{id}
+					{name}
+					{adr_1}
+					{adr_2}
+					{city}
+					{eircode}
+					{contact}
+					{phone}
+					{email}
+					{website}
+					{info}
+					on:edit={()=>findItemById(id)}
+					on:click={openDeleteConfirmModal}
+				/>
 			{/each}
 		</div>
 	</section>
@@ -153,32 +138,5 @@
 		grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 		grid-template-rows: auto;
 		grid-gap: 1rem;
-	}
-
-	.db-item {
-		display: flex;
-		flex-grow: 1;
-		flex-basis: 400px;
-		flex-direction: column;
-		justify-content: space-between;
-		border: #ccc 1px solid;
-		padding: 1rem;
-		border-radius: 0.25rem;
-	}
-	.db-item h2 {
-		margin-bottom: 1rem;
-	}
-	.dsb-venue--header {
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 1rem;
-		line-height: 1.2;
-	}
-	/* .dsb-venue--body {
-		margin-bottom: 1rem;
-		line-height: 1.4;
-	} */
-	.dsb-venue--footer {
-		text-align: right;
 	}
 </style>
