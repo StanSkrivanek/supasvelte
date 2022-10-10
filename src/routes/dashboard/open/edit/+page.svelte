08d7385ff3cea96425b21304634ef7f41e4773f9
Stan Skrivanek <s.skrivanek@icloud.com>
2022-10-10 10:49:10 +0100

    UPDATE: style for Open create - edit

--- src/routes/dashboard/open/edit/+page.svelte
+++ src/routes/dashboard/open/edit/+page.svelte
@@ -71,123 +71,131 @@
 	}
 </script>
 
-<h2>Open new course</h2>
+<article>
+	<div class="dash-header">
+		<h1>Update Open Course</h1>
+	</div>
+	<section>
+		<form on:submit|preventDefault={handleSubmit} action="" id="open-course" method="POST">
+			<!-- <form on:submit|preventDefault={handleSubmit} action="dashboard/open" method="POST"> -->
+			<!-- <form on:submit|preventDefault={handleSubmit} method="POST"> -->
+			<div class="form-select">
+				<SelectFromDb
+					label="Course"
+					db_table={'courses'}
+					tb_col={'title'}
+					bind:selectedListOption={values.course}
+				/>
+				<SelectFromDb
+					label="Venue"
+					db_table={'venues'}
+					tb_col={'name'}
+					bind:selectedListOption={values.venue}
+				/>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<label for="group">Group</label>
+					<select name="group" id="group" bind:value={values.groupNo}>
+						<option value="">Select group</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+					</select>
+				</div>
+				<div class="form-select__w">
+					<label for="weekday">Week Day</label>
+					<select name="weekday" id="weekday" bind:value={values.weekday}>
+						<option value="">Select day</option>
+						<option value="Monday">Monday</option>
+						<option value="Tuesday">Tuesday</option>
+						<option value="Wednesday">Wednesday</option>
+						<option value="Thursday">Thursday</option>
+						<option value="Friday">Friday</option>
+						<option value="Saturday">Saturday</option>
+						<option value="Sunday">Sunday</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<label for="date-in">Starts Date</label>
+					<input type="date" name="date-in" id="date-in" bind:value={values.date_in} />
+				</div>
+				<div class="form-select__w">
+					<label for="date-end">Ends Date</label>
+					<input type="date" name="date-end" id="date-end" bind:value={values.date_end} />
+				</div>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<label for="time-in">Start Time</label>
+					<input type="time" name="time-in" id="time-in" bind:value={values.time_in} />
+				</div>
+				<div class="form-select__w">
+					<label for="time-end">End Time</label>
+					<input type="time" name="time-end" id="time-end" bind:value={values.time_end} />
 
-<form on:submit|preventDefault={handleSubmit} action="" id="open-course" method="POST">
-	<!-- <form on:submit|preventDefault={handleSubmit} action="dashboard/open" method="POST"> -->
-	<!-- <form on:submit|preventDefault={handleSubmit} method="POST"> -->
-	<div class="form-select">
-		<SelectFromDb
-			label="Course"
-			db_table={'courses'}
-			tb_col={'title'}
-			bind:selectedListOption={values.course}
-		/>
-		<SelectFromDb
-			label="Venue"
-			db_table={'venues'}
-			tb_col={'name'}
-			bind:selectedListOption={values.venue}
-		/>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<label for="group">Group</label>
-			<select name="group" id="group" bind:value={values.groupNo}>
-				<option value="">Select group</option>
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-			</select>
-		</div>
-		<div class="form-select__w">
-			<label for="weekday">Week Day</label>
-			<select name="weekday" id="weekday" bind:value={values.weekday}>
-				<option value="">Select day</option>
-				<option value="Monday">Monday</option>
-				<option value="Tuesday">Tuesday</option>
-				<option value="Wednesday">Wednesday</option>
-				<option value="Thursday">Thursday</option>
-				<option value="Friday">Friday</option>
-				<option value="Saturday">Saturday</option>
-				<option value="Sunday">Sunday</option>
-			</select>
-		</div>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<label for="date-in">Starts Date</label>
-			<input type="date" name="date-in" id="date-in" bind:value={values.date_in} />
-		</div>
-		<div class="form-select__w">
-			<label for="date-end">Ends Date</label>
-			<input type="date" name="date-end" id="date-end" bind:value={values.date_end} />
-		</div>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<label for="time-in">Start Time</label>
-			<input type="time" name="time-in" id="time-in" bind:value={values.time_in} />
-		</div>
-		<div class="form-select__w">
-			<label for="time-end">End Time</label>
-			<input type="time" name="time-end" id="time-end" bind:value={values.time_end} />
-
-			<!-- <div class="form-select__w">
+					<!-- <div class="form-select__w">
         <label for="time-end">Duration</label>
         <input type="time" name="time-end" id="time-end" />
       </div> -->
-		</div>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<label for="price">Price</label>
-			<input type="number" name="price" id="price" bind:value={values.price} />
-		</div>
-		<div class="form-select__w">
-			<label for="formAttachment">Form Attachment</label>
-			<input
-				type="file"
-				name="formAttachment"
-				id="formAttachment"
-				bind:value={values.formAttachment}
-			/>
-		</div>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<label for="applyWillOpen">Apply Will Open</label>
-			<input
-				type="date"
-				name="applyWillOpen"
-				id="applyWillOpen"
-				bind:value={values.applyWillOpen}
-			/>
-		</div>
-		<div class="form-select__w">
-			<label for="applyWillClose">Apply Will Close</label>
-			<input
-				type="date"
-				name="applyWillClose"
-				id="applyWillClose"
-				bind:value={values.applyWillClose}
-			/>
-		</div>
-	</div>
-	<div class="form-2col-section">
-		<div class="form-select__w">
-			<SwitchRoundy label={'Show on website'} on:click={isActive} />
-		</div>
-	</div>
+				</div>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<label for="price">Price</label>
+					<input type="number" name="price" id="price" bind:value={values.price} />
+				</div>
+				<div class="form-select__w">
+					<label for="formAttachment">Form Attachment</label>
+					<input
+						type="file"
+						name="formAttachment"
+						id="formAttachment"
+						bind:value={values.formAttachment}
+					/>
+				</div>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<label for="applyWillOpen">Apply Will Open</label>
+					<input
+						type="date"
+						name="applyWillOpen"
+						id="applyWillOpen"
+						bind:value={values.applyWillOpen}
+					/>
+				</div>
+				<div class="form-select__w">
+					<label for="applyWillClose">Apply Will Close</label>
+					<input
+						type="date"
+						name="applyWillClose"
+						id="applyWillClose"
+						bind:value={values.applyWillClose}
+					/>
+				</div>
+			</div>
+			<div class="form-2col-section">
+				<div class="form-select__w">
+					<SwitchRoundy label={'Show on website'} on:click={isActive} />
+				</div>
+			</div>
 
-	<div class="btns__c">
-		<button type="button" class="danger" on:click={cancel}>cancel</button>
-		<button class="info">save</button>
-	</div>
-	<pre>{JSON.stringify(values, null, 2)}</pre>
-</form>
+			<div class="btns__c">
+				<button type="button" class="danger" on:click={cancel}>cancel</button>
+				<button class="info">save</button>
+			</div>
+			<pre>{JSON.stringify(values, null, 2)}</pre>
+		</form>
+	</section>
+</article>
 
 <style>
+	section{
+		padding: 1rem;
+	}
 	.form-2col-section {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
