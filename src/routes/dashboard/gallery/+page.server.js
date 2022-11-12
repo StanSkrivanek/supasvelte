cf0a520c5b89904582e63ce607bd8ad8a1f1ee44
Stan Skrivanek <s.skrivanek@icloud.com>
2022-11-12 23:24:11 +0000

    ADD: Image Gallery - basics

---
+++ src/routes/dashboard/gallery/+page.server.js
@@ -0,0 +1,27 @@
+import { supabase } from '$lib/supabase/supabaseClient';
+
+export async function load() {
+	// get all images from and their public url db
+
+	const { data: images, error } = await supabase.storage
+		.from('images')
+		.list('rte/course', { offset: 1, limit: 100 });
+	// get public url for each image
+	const imageUrls = images.map((image) => {
+		// get public URL for each image
+
+		let imgUrl = supabase.storage.from('images').getPublicUrl(`rte/course/${image.name}`)
+			.data.publicUrl;
+		// if (image.name !== '.emptyFolderPlaceholder') {
+		return { name: image.name, id: image.id, url: imgUrl };
+		// }
+	});
+
+	console.log('🚀 ~ file: +page.js ~ line 12 ~ load ~ images', images, imageUrls);
+
+	if (error) {
+		console.log(error);
+	}
+
+	return { imageUrls };
+}
--- src/routes/dashboard/gallery/+page.svelte
+++ src/routes/dashboard/gallery/+page.svelte
@@ -1,1 +1,53 @@
+
+<script>
+	export let data;
+	let { imageUrls } = data;
+	console.log('🚀 ~ file: +page.svelte ~ line 5 ~ imageUrls', imageUrls)
+
+
+</script>
 <h1>Gallery</h1>
+
+<!-- create grid gallery for data -->
+<div class="grid">
+  {#each imageUrls as item}
+    <div class="db-item">
+      <div class="db-item__img">
+        <img src={item.url} alt={item.name} />
+      </div>
+      <div class="db-item__info">
+        <p>{item.name}</p>
+      </div>
+      
+      <button>delete</button>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 1rem;
+  }
+  .db-item {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .db-item__img {
+    width: 100%;
+    height: 200px;
+    overflow: hidden;
+  }
+  .db-item__img img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .db-item__info {
+    padding: 1rem;
+  }
+</style>
